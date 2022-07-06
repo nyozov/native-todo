cb8131b5ea4d44769de6b636208d7586127a80b6
nikolay <n_yozov@hotmail.com>
2022-07-06 17:40:35 -0400

    redux setup, install tw

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
-import store from './store'
+import HomeScreen from "./screens/HomeScreen";
+import { store } from './store'
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Building</Text>
-        <StatusBar style="auto" />
-      </View>
+      <HomeScreen/>
     </Provider>
   );
 }
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -13,5 +13,24 @@ export const navSlice = createSlice({
     setOrigin: (state, action) => {
       state.origin = action.payload;
     },
+
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
   },
 });
+
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
+
+// Selectors
+
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
